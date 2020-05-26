33e5e2d099fe7b60b617c7604f861ab423810ca5
Abdu-w <61803409+Abdu-w@users.noreply.github.com>
2020-05-26 04:38:45 -0400

    Update App.js

--- Mod3/react-joke-app/src/App.js
+++ Mod3/react-joke-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import axios from 'axios'
+
 import './App.css';
 import InitialCall from './components/InitialCall';
 import Nav from './components/Navbar'
